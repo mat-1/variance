58db09d6c722996d73af47d603b76793b4b3589a
mat <git@matdoes.dev>
2023-08-11 03:23:10 -0500

    change font to atkinson hyperlegible

--- src/font.js
+++ src/font.js
@@ -1,5 +1,3 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/inter/variable.css';
+// import '@fontsource/atkinson-hyperlegible/300.css';
+import '@fontsource/atkinson-hyperlegible/400.css';
+import '@fontsource/atkinson-hyperlegible/700.css';
